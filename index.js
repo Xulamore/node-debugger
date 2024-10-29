2b085af38e3e384f4842e9c4d87d99fafe78f94b
Xulamore <warkentin.jury@gmail.com>
2024-10-29 16:04:34 +0100

    46. Commit: Hausaufgabe - 29.10.2024

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 // index.js
-var items = [1, "2", 3, "four", 5, null, undefined, 6]
-const results = []
+var items = [1, "2", 3, "four", 5, null, undefined, 6];
+const results = [];
 
-items.foreach((item) => {
-    if(typeof item === 'number')
-        if(item % 2 === 0) results.push(item* 2)
-
-    else if(item === null)
-    console.log("Found a null item")
-
-    else 
-    {
-        result.push(parseInt(item))
+items.forEach((item) => {
+    if (typeof item === 'number') {
+        if (item % 2 === 0) results.push(item * 2);
+    } else if (item === null) {
+        conso.log("Found a null item");
+    } else {
+        results.push(parseInt(item) || 0); 
     }
 });
 
-console.log("Original items:", items)
-console.log("Filtered and transformed items:", results)
+console.log("Original items:", items);
+console.log("Filtered and transformed items:", results);
+
+/* 
+foreach wurde zu forEach korrigiert.
+result.push wurde zu results.push korrigiert.
+parseInt(item) wurde mit || 0 ergänzt, um NaN zu vermeiden, wenn parseInt fehlschlägt.
+*/
